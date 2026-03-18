fa15343024c329ba1991f8be08bbc2fa5fb0a60d
jimmywu447 <jimmywu447@gmail.com>
2026-03-18 23:04:28 +0800

    Refactor WordBank.cpp with comments for clarity
    Added comments in Chinese to clarify functionality.

--- WordBank.cpp
+++ WordBank.cpp
@@ -1,26 +1,26 @@
 #include "WordBank.h"
 
 WordBank::WordBank() {
-    words = {"computer", "science", "apple", "banana", "keyboard", "system", "coding"};
+    words = {"computer", "science", "apple", "banana", "keyboard", "system", "coding"};    //題庫
     srand(time(0));
 }
 
 void WordBank::generateQuestion() {
     int index = rand() % words.size();
-    answer = words[index];
-    maskedWord = answer;
+    answer = words[index];                         //正確答案
+    maskedWord = answer;                           //答案暫存
 
-    int len = answer.length();
+    int len = answer.length();                     //答案長度
     
-    if (len < 5) {
+    if (len < 5) {                                //長度<5 隨機挖空1~2個字母
         int holes = (rand() % 2) + 1;
         for(int i = 0; i < holes; i++) {
             maskedWord[rand() % len] = '_';
         }
-    } else if (len % 2 != 0) {
+    } else if (len % 2 != 0) {                    //長度是奇數 挖空第1個和第4個字母
         maskedWord[0] = '_';
         maskedWord[3] = '_';
-    } else {
+    } else {                                       //長度是偶數 挖空第2個和第5個字母
         maskedWord[1] = '_';
         maskedWord[4] = '_';
     }
