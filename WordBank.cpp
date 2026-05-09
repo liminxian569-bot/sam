6a3a1143f531a9a803c1e55da1d677502d6f0225
jimmywu447 <jimmywu447@gmail.com>
2026-05-09 08:54:51 +0800

    Load words and meanings from file in WordBank
    Refactor WordBank class to load words from a file and include categories.

--- WordBank.cpp
+++ WordBank.cpp
@@ -1,34 +1,53 @@
 #include "WordBank.h"
+#include <ctime>
+#include <cstdlib>
 
 WordBank::WordBank() {
-    
-    words = { "computer", "science", "apple", "banana", "keyboard", "system", "coding" };
+    loadFromFile();
+}
 
-
-    meanings = { "電腦", "科學", "蘋果", "香蕉", "鍵盤", "系統", "程式設計" };
-
-    srand(time(0));
+void WordBank::loadFromFile() {
+    ifstream file("words.txt");
+    string line;
+    if (file.is_open()) {
+        while (getline(file, line)) {
+            stringstream ss(line);
+            string cat, eng, chi;
+            if (getline(ss, cat, '|') && getline(ss, eng, '|') && getline(ss, chi)) {
+                categories.push_back(cat);
+                words.push_back(eng);
+                meanings.push_back(chi);
+            }
+        }
+        file.close();
+    } else {
+        categories.push_back("default");
+        words.push_back("error");
+        meanings.push_back("讀檔失敗");
+    }
 }
 
 void WordBank::generateQuestion() {
+    if (words.empty()) return;
+
     int index = rand() % words.size();
     answer = words[index];
-    currentMeaning = meanings[index]; 
+    meaning = meanings[index];
+    category = categories[index];
     maskedWord = answer;
 
     int len = answer.length();
-
-   
+    
     if (len < 5) {
-        int holes = (rand() % 2) + 1;
-        for (int i = 0; i < holes; i++) {
+        int numHoles = (rand() % 2) + 1;
+        for(int i = 0; i < numHoles; i++) {
             maskedWord[rand() % len] = '_';
         }
-    }
+    } 
     else if (len % 2 != 0) {
         maskedWord[0] = '_';
         maskedWord[3] = '_';
-    }
+    } 
     else {
         maskedWord[1] = '_';
         maskedWord[4] = '_';
